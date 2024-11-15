ce78bed884f83cf9d661f797a0a6f5b9481157d7
James Parrott <japarrott@shaw.ca>
2024-11-15 14:28:11 -0700

    Third random madlib

---
+++ sample_madlibs/test3.py
@@ -0,0 +1,40 @@
+def madlib():
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
+    noun6 = input("Noun: ")
+    noun_plural = input("Noun plural: ")
+    body_part = input("Body part: ")
+    body_part2 = input("Body part: ")
+    verb = input("Verb: ")
+    verb_past = input("Verb (past tense): ")
+    verb_past2 = input("Verb (past tense): ")
+    spell1 = input("Spell: ")
+    spell2 = input("Spell: ")
+
+
+    madlib = f"A {adj1} glow burst suddenly across the enchanted sky above them as an edge of \
+dazzling sun appeared over the sill of the nearest {noun1}. The light hit both of their {body_part} \
+at the same time, so that Jon Irenicus was suddenly a flaming {noun2}. Charname heard the high voice \
+shriek as he too {verb_past} his best hope to the heavens, pointing Bodhi’s {noun3}:\n\
+\"{spell1}!\"\n\
+\"{spell2}!\"\n\
+The bang was like a cannon blast, and the {adj2} flames that erupted between them, \
+at the dead center of the circle they had been treading, marked the point where the \
+{noun_plural} collided. Charname saw Irenicus’ {adj3} jet meet his own spell, saw the Bhaalspawn's soul \
+fly high, dark against the sunrise, spinning across the enchanted ceiling \
+, spinning through the air toward the master it would not {verb}, who had \
+come to take full possession of it at last. And Charname, with the unerring skill of a Seeker, \
+caught the {noun4} in his free hand as Irenicus fell backward, arms splayed, the slit pupils \
+of the {adj4} {body_part2} rolling upward. Irenicus hit the floor with a mundane finality, his body \
+feeble and shrunken, the white hands empty, the snakelike face vacant and unknowing. Irenicus \
+was dead, {verb_past2} by his own rebounding {noun5}, and Charname stood with two souls in his hands, \
+staring down at his enemy’s {noun6}."
+
+    print(madlib)
