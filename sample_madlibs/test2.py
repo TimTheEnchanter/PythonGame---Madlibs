bdfe43c28fa0dd8c12b4fbe61f1447423d4a1627
James Parrott <japarrott@shaw.ca>
2024-11-15 13:53:59 -0700

    second random madlib

---
+++ sample_madlibs/test2.py
@@ -0,0 +1,21 @@
+def madlib():
+    time_of_day = input("Time of Day: ")
+    influencer_adj = input("Influencer Adjective: ")
+    color = input("Color: ")
+    verb_past_tense = input("Verb (past tense): ")
+    #food = input("Food: ")
+    noun1 = input("Noun: ")
+    noun_plural_1 = input("Noun (plural): ")
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+
+
+    madlib = f"It was a {adj1} winter {time_of_day} when we realized that the vaccine to stop \
+spread of the disease did not work. Instead, it produced PRANK YOUTUBERS!!! They were thirsty for {influencer_adj} \
+and the streets were lined with these monsters holding {noun_plural_1} in their hands. \
+Their eyes were {color} with hunger as they {verb_past_tense} around the city looking for {influencer_adj}. \
+They were {adj2} and {adj3}, unknowing how to act in this human world... except obtain {influencer_adj}!!!! \
+That was their sole mission and they were dedicated towards achieving it for the sake of {noun1}."
+    
+    print(madlib)
